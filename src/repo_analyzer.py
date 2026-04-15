5896712e8daa49db270d050ddaff44f7a99eb693
Oskar Jonsson <oskarjonsson.dev@gmail.com>
2026-04-15 16:32:37 +0200

    update analyzer to analyze branching behavior

--- src/repo_analyzer.py
+++ src/repo_analyzer.py
@@ -1,22 +1,68 @@
+from git import Repo
 from pydriller import Repository
 
 
 class RepoAnalyzer:
     def analyze_repo(self, repo_path: str) -> dict:
-        authors = {}
+        return self.analyze_branches(repo_path)
+
+    def analyze_branches(self, repo_path: str) -> dict:
+        repo = Repo(repo_path)
+
+        branches = [branch.name for branch in repo.branches]
+
+        is_single_branch = len(branches) == 1
+
+        total_commits = 0
+        merge_commits = 0
 
         for commit in Repository(repo_path).traverse_commits():
-            name = commit.author.name
+            total_commits += 1
 
-            if name not in authors:
-                authors[name] = {
-                    "commits": 0,
-                    "insertions": 0,
-                    "deletions": 0
-                }
+            # Merge commit has at least two parent commits
+            # [Branch A] commit 1 -> MERGE_COMMIT
+            # [Branch B] commit 1 -> MERGE_COMMIT
+            if len(commit.parents) > 1:
+                merge_commits += 1
 
-            authors[name]["commits"] += 1
-            authors[name]["insertions"] += commit.insertions
-            authors[name]["deletions"] += commit.deletions
+        has_branching_activity = merge_commits > 0 or len(branches) > 1
 
-        return authors
+        branch_commit_counts = {}
+
+        for branch in branches:
+            commits = list(repo.iter_commits(branch))
+            branch_commit_counts[branch] = len(commits)
+
+        avg_commits_per_branch = 0
+
+        if len(branch_commit_counts) > 0:
+            avg_commits_per_branch = (
+                sum(branch_commit_counts.values()) / len(branch_commit_counts)
+            )
+
+        merge_ratio = 0
+
+        if total_commits > 0:
+            merge_ratio = merge_commits / total_commits
+
+        if is_single_branch and merge_commits == 0:
+            strategy = "no_branching"
+        elif len(branches) > 1:
+            strategy = "multi_branch"
+        elif merge_commits > 0:
+            strategy = "merged_branches"
+        else:
+            strategy = "unknown"
+
+        return {
+            "count": len(branches),
+            "names": branches,
+            "is_single_branch": is_single_branch,
+            "has_branching_activity": has_branching_activity,
+            "strategy": strategy,
+            "total_commits": total_commits,
+            "merge_commits": merge_commits,
+            "merge_ratio": merge_ratio,
+            "commits_per_branch": branch_commit_counts,
+            "avg_commits_per_branch": avg_commits_per_branch
+        }
